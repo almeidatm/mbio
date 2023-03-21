d73c160348e48c4ade18e2d0f0e0ec210e098778
Thiago Almeida <machado.almeida.tm@gmail.com:wq>
2023-03-21 14:06:12 +0000

    finished test case draft

--- tests/a-class-price-range.spec.ts
+++ tests/a-class-price-range.spec.ts
@@ -1,5 +1,26 @@
 import { test, expect } from '@playwright/test';
 
+import fs from 'fs/promises';
+
+async function writeToFile(minPrice: number, maxPrice: number) {
+  const formatter = new Intl.NumberFormat('en-UK', {
+    style: 'currency',
+    currency: 'GBP',
+  })
+  const min = formatter.format(minPrice);
+  const max = formatter.format(maxPrice);
+
+  try {
+    const content = `Min price is ${min} \nMax price is ${max}`;
+    await fs.writeFile('prices.txt', content);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const EXPECTED_MIN_PRICE = 15000;
+const EXPECTED_MAX_PRICE = 60000;
+
 test('Validate A Class models price are between £15,000 and £60,000', async ({ page }) => {
   await page.goto('');
   await page.getByRole('button', { name: 'Agree to all' }).click();
@@ -17,9 +38,23 @@ test('Validate A Class models price are between £15,000 and £60,000', async ({
   await page.locator('.owc-stage-cta-buttons__button').getByText(' Build your car ').click();
   await page.waitForURL('passengercars/mercedes-benz-cars/car-configurator.html/motorization/CCci/GB/en/bm/1770122,1770512,1770542,1770842,1770872');
 
-  // Filter by Fuel type “Diesel”
-  // Take and save a screenshot of the results
-  // Save the value “£” of the highest and lowest price results in a text file
+  await page.getByText('Fuel type').click();
+
+  await page.getByLabel(' Diesel ').check({ force: true });
+  await expect(page.locator('.wb-multi-select-control__counter')).toHaveText('1');
+  await page.getByText('Fuel type').click();
+  // await page.screenshot({ path: 'screenshot.png', fullPage: true });
+  await page.locator('cc-motorization').screenshot({ path: 'screenshot.png' });
+
+  const texts = await page.locator('.cc-motorization-header__price--with-environmental-hint').allInnerTexts();
+  const results = texts.map(element => {
+    return parseFloat(element.replace('£', '').replace(',', ''));
+  });
+  const minPrice = Math.min(...results);
+  const maxPrice = Math.max(...results);
+  expect(minPrice).toBeGreaterThan(EXPECTED_MIN_PRICE);
+  expect(maxPrice).toBeLessThan(EXPECTED_MAX_PRICE);
+  writeToFile(minPrice, maxPrice);
 
 });
 
